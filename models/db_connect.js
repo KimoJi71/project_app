b57809fb9a24b4838104a39d022bf9d027baa594
KimoJi71 <t107ab0048@gmail.com>
2021-08-05 16:32:07 +0800

    connect 遠端DB

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,9 +1,11 @@
+var config = require('../config/development_config');
 var mysql = require('mysql2');
+
 var pool = mysql.createPool({
-  host:'localhost',
-  user:'root',
-  password:'taiwangirl8971',
-  database:'project_app',
+  host:config.mysql.host,
+  user:config.mysql.user,
+  password:config.mysql.password,
+  database:config.mysql.database,
   waitForConnections:true,
   connectionLimit:10,
   queueLimit:0
