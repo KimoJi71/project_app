881b6dba71b53d285266f49b62203e37a143c739
KimoJi71 <t107ab0048@gmail.com>
2021-09-13 16:46:30 +0800

    add productCollect apis

--- routes/collection.js
+++ routes/collection.js
@@ -2,16 +2,6 @@ var express = require('express')
 var db = require('../models/db_connect')
 var router = express.Router()
 
-//取得文章被收藏數
-router.get('/posts/:postNum', (req, res) => {
-    const {postNum} = req.params
-
-    db.query(`SELECT COUNT(*) AS Number FROM postCollect WHERE postNum = ${postNum}`)
-    .then((result) => {
-        res.json(result[0])
-    })
-})
-
 //取得用戶收藏的文章
 router.get('/posts-list/:memNum', (req, res) => {
     const {memNum} = req.params
@@ -20,6 +10,16 @@ router.get('/posts-list/:memNum', (req, res) => {
     FROM (postCollect INNER JOIN posts ON postCollect.postNum = posts.postNum AND postCollect.memNum = ${memNum})
     INNER JOIN members ON posts.memNum = members.memNum 
     ORDER BY posts.postCreateAt DESC`)
+    .then((result) => {
+        res.json(result[0])
+    })
+})
+
+//取得文章被收藏數
+router.get('/posts/:postNum', (req, res) => {
+    const {postNum} = req.params
+
+    db.query(`SELECT COUNT(*) AS Number FROM postCollect WHERE postNum = ${postNum}`)
     .then((result) => {
         res.json(result[0])
     })
@@ -50,13 +50,51 @@ router.delete('/posts/:postNum/:memNum', (req, res) => {
     })
 })
 
-//取得商品被收藏數
-
 //取得用戶收藏的商品
+router.get('/products-list/:memNum', (req, res) => {
+    const {memNum} = req.params
+
+    db.query(`SELECT products.*
+    FROM (productCollect INNER JOIN products ON productCollect.proNum = products.proNum AND productCollect.memNum = ${memNum})`)
+    .then((result) => {
+        res.json(result[0])
+    })
+})
+
+//取得商品被收藏數
+router.get('/products/:proNum', (req, res) => {
+    const {proNum} = req.params
+
+    db.query(`SELECT COUNT(*) AS Number FROM productCollect WHERE proNum = ${proNum}`)
+    .then((result) => {
+        res.json(result[0])
+    })
+})
 
 //收藏商品
+router.post('/products/:proNum', (req, res) => {
+    const {proNum} = req.params
+    const memNum = req.body.memNum
+    
+    db.query(`INSERT INTO productCollect (proNum, memNum) VALUES ('${proNum}', '${memNum}')`)
+    .then((result) => {
+        res.json({
+            message: '成功收藏了商品'
+        })
+    })
+})
 
 //取消收藏商品
+router.delete('/products/:proNum/:memNum', (req, res) => {
+    const {proNum, memNum} = req.params
+
+    db.query(`DELETE FROM productCollect WHERE proNum = '${proNum}' AND memNum = '${memNum}'`)
+    .then((result) => {
+        res.json({
+            message: '成功取消商品收藏'
+        })
+    })
+})
 
 //取得業務員被收藏數
 
