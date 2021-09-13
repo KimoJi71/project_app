d80c911f94730ed6c78b01b004a2b50de90c3f47
KimoJi71 <t107ab0048@gmail.com>
2021-09-13 11:48:22 +0800

    add some apis connect with productLike

--- routes/product.js
+++ routes/product.js
@@ -19,6 +19,59 @@ router.get('/', (req, res) => {
   })
 })
 
+//取得商品按讚數
+router.get('/liked/:proNum', (req, res) => {
+  const {proNum} = req.params
+
+  db.query(`SELECT COUNT(*) AS Number FROM productLike WHERE proNum = ${proNum}`)
+  .then((result) => {
+    res.json(result[0])
+  })
+})
+
+//商品按讚
+router.post('/liked/:proNum', (req, res) => {
+  const {proNum} = req.params
+  const memNum = req.body.memNum
+
+  db.query(`INSERT INTO productLike (proNum, memNum) VALUES ('${proNum}', '${memNum}')`)
+  .then((result) => {
+    res.json({
+      message: '成功為商品按讚'
+    })
+  })
+})
+
+//取消商品按讚
+router.delete('/liked/:proNum/:memNum', (req, res) => {
+  const {proNum, memNum} = req.params
+
+  db.query(`DELETE FROM productLike WHERE proNum = '${proNum}' AND memNum = '${memNum}'`)
+  .then((result) => {
+    res.json({
+      message: '成功取消商品按讚'
+    })
+  })
+})
+
+//檢查用戶是否有按讚
+router.get('/liked/:proNum/:memNum', (req, res) => {
+  const {proNum, memNum} = req.params
+
+  db.query(`SELECT * FROM productLike WHERE proNum = ${proNum} AND memNum = ${memNum}`)
+    .then((result) => {
+      if(result[0][0] !== undefined) {
+        res.json({
+            message: '已按讚'
+        })
+      } else {
+        res.status(400).json({
+          message: '未按讚'
+        })
+      }
+    })
+})
+
 //收藏
 
 module.exports = router
